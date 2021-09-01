56a9e296bc4f7d2a0f8678ff81da1545d8ba5217
Jennifer Jung <jenjung2194@gmail.com>
2021-09-01 14:18:36 -0400

    the route succesfully fulfills an order and sends back the modified data

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+var bodyParser = require('body-parser')
 const app = express();
 
 const url = `https://${process.env.API_KEY}:${process.env.PASSWORD}@${process.env.SHOP}.myshopify.com`;
 
 app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+app.use(express.json());
 app.get('/api/orders', (req, res) => {
   axios.get(url + "/admin/api/2021-07/orders.json?status=any")
     .then(response => {
@@ -18,11 +22,27 @@ app.get('/api/orders', (req, res) => {
 });
 
 app.post('/api/fulfillOrder', (req, res) => {
-  const orderId = req.body.orderId;
-  axios.put(url + `/admin/api/2021-07/orders/${orderId}.json`)
-    .then(res => {
-      res.json("fulfilled!!")
+  console.log(req.body)
+  const { orderId, locationId } = req.body
+  axios.post(url + `/admin/api/2021-07/orders/${orderId}/fulfillments.json`, {
+    "fulfillment": {
+      "location_id": locationId,
+      "tracking_number": "123456789",
+      "tracking_urls": [
+        "https://shipping.xyz/track.php?num=123456789",
+        "https://anothershipper.corp/track.php?code=abc"
+      ],
+      "notify_customer": false
+    }
+  })
+    .then(response => {
+      axios.get(url + "/admin/api/2021-07/orders.json?status=any")
+      .then(response => {
+        // console.log("response: ", response.data)
+        res.json(response.data);
+      })
     })
+    .catch(e => console.log(e))
 })
 
 const port = 5000;
