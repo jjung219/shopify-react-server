c3097fb7af42d847b78fab5cfe935744d7947cac
Jennifer Jung <jenjung2194@gmail.com>
2021-09-01 11:58:12 -0400

    set up route to get orders list and to check order as fulfilled

--- server.js
+++ server.js
@@ -5,16 +5,26 @@ const cors = require('cors');
 const axios = require('axios');
 const app = express();
 
+const url = `https://${process.env.API_KEY}:${process.env.PASSWORD}@${process.env.SHOP}.myshopify.com`;
+
 app.use(cors())
 app.get('/api/orders', (req, res) => {
-  axios.get('https://c2451952ff6116701c3a4fd323fb3c93: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@jj-store-123.myshopify.com/admin/api/2021-07/orders.json')
+  axios.get(url + "/admin/api/2021-07/orders.json?status=any")
     .then(response => {
       // console.log("response: ", response.data)
       res.json(response.data);
     })
 
 });
-//https://c2451952ff6116701c3a4fd323fb3c93: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@jj-store-123.myshopify.com/admin/api/2021-07/orders.json
+
+app.post('/api/fulfillOrder', (req, res) => {
+  const orderId = req.body.orderId;
+  axios.put(url + `/admin/api/2021-07/orders/${orderId}.json`)
+    .then(res => {
+      res.json("fulfilled!!")
+    })
+})
+
 const port = 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
